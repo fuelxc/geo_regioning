5080134a164a9532e5f781baa29895aca761d877
Eric Harrison <eharrison@classifiedadventures.com>
2011-04-14 16:12:56 +1000

    typo in the migration

--- lib/db/migrate/20110414145000_add_left_and_right.rb
+++ lib/db/migrate/20110414145000_add_left_and_right.rb
@@ -1,7 +1,7 @@
 class AddLeftAndRight < ActiveRecord::Migration
   def self.up
     add_column :geo_regioning_levels, :lft, :integer, :null => false, :default => 0
-    add_column :geo_regioning_levels, :rgt, :intefer, :null => false, :default => 0
+    add_column :geo_regioning_levels, :rgt, :integer, :null => false, :default => 0
   end
 
   def self.down
