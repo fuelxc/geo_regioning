074b9265a233d1121526e952e8d2e805207828d1
Eric Harrison <eharrison@classifiedadventures.com>
2011-07-13 15:19:26 +1000

    make sure it was a digit for a level

--- lib/app/models/geo_regioning/country.rb
+++ lib/app/models/geo_regioning/country.rb
@@ -29,7 +29,7 @@ class GeoRegioning::Country < GeoRegioning::Base
   def level_name_depth_map
     @level_name_depth_map if @level_name_depth_map
     levels_hash = {}
-    GeoRegioning.config['country_definitions'][self.iso_3166].keys.select{|k| k =~ /^\d+$/}.map{|key| levels_hash[GeoRegioning.config['country_definitions'][self.iso_3166][key]['name']] = key}
+    GeoRegioning.config['country_definitions'][self.iso_3166].keys.select{|k| k.to_s.to_i == k}.map{|key| levels_hash[GeoRegioning.config['country_definitions'][self.iso_3166][key]['name']] = key}
     @level_name_depth_map = levels_hash
   end
 
